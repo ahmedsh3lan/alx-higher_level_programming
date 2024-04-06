720c78bd308bc040c1c9120266da15b278a01244
Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>
2024-04-07 01:54:16 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+"""Sends a POST request to a given URL with a given email.
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
+
+
 import sys
+import urllib.parse
 import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url)
+    request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(response.read().decode("utf-8"))
